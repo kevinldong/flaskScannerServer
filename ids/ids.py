32533115806b0c89a2bde526b85e1c1b77ae4bd1
kevinldong <110069590+kevinldong@users.noreply.github.com>
2022-09-11 05:13:55 -0400

    Update ids.py

--- ids/ids.py
+++ ids/ids.py
@@ -1,18 +1,13 @@
-import requests
+import vt
 
 def virustotal_file_scan(file: str):
-    url = "https://www.virustotal.com/api/v3/files"
-
+    client = vt.Client("<62f38e8e1c83d0c31ddaf4e93cc260c9e0aaaba9c72b949e4492d3e3a71c4a79>")
     files = {"file": open(file, "rb")}
-    headers = {
-        "Accept": "application/json",
-        "x-apikey": 'api key',
-        "Content-Type": "multipart/form-data"
-    }
 
-    response = requests.post(url, headers=headers)
+    with open(files, "rb") as f:
+        analysis = client.scan_file(f, wait_for_completion=True)
 
-    if response.text == 200:
+    if analysis == 200:
         return True
-    elif response.text == 400:
+    else:
         return False
