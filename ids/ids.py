47488c6f92eb39197c6dc5e1a3c10bf84c5af036
kevinldong <110069590+kevinldong@users.noreply.github.com>
2022-09-15 21:06:56 -0400

    Update ids.py

--- ids/ids.py
+++ ids/ids.py
@@ -1,33 +1,23 @@
 import vt
 from io import BufferedReader
 
+
 def virustotal_file_scan(file: str):
-    client = vt.Client("<API KEY>")
-
-    # Wrap file in a BufferedReader to pass the API type check
+    client = vt.Client("API KEY")
     scan_file = BufferedReader(file)
-
-    # Scan file
     analysis = client.scan_file(scan_file, wait_for_completion=True)
 
-    # Get detection stats
-    undetected_count = analysis.stats['undetected']
-    harmless_count = analysis.stats['harmless']
     suspicious_count = analysis.stats['suspicious']
     malicious_count = analysis.stats['malicious']
 
-    # Get completion status
-    completion = analysis.status
+    client.close()
 
-    # TODO: use above to make decision of whether to save
-    while completion == 'completed':
-        if undetected_count == 0:
-            return False
-        elif harmless_count == 0:
-            return False
-        elif suspicious_count > 0:
-            return False
-        elif malicious_count > 0:
-            return False
-        else:
-            return True
+    print("suspicious count = " + str(suspicious_count))
+    print(f'{malicious_count=}')
+
+    if suspicious_count > 5:
+        return False
+    elif malicious_count > 2:
+        return False
+    else:
+        return True
